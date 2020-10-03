450e3d76f41553413477742dd2373c11ca8c88ea
sianbalagan <sianbalagan@paypal.com>
2020-10-03 09:38:12 -0700

    Modifying the "div-id"

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CoreClass from './Weather';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CoreClass />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('own app')
 );
 
 // If you want your app to work offline and load faster, you can change
